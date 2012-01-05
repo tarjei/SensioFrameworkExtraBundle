62cf316d582af5a15702434d79ba6d227c429bb5
Tarjei Huse <tarjei@scanmine.com>
2012-01-05 14:59:10 +0100

    This commit changes the DoctrineParamConverter to handle paths like:
    /{user}/{group}
    
    by beeing able to assign which request variable should be used for a
    given provider.
    
    It also adds support for defining how the object should be queried.

--- Request/ParamConverter/DoctrineParamConverter.php
+++ Request/ParamConverter/DoctrineParamConverter.php
@@ -5,10 +5,7 @@ namespace Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\DoctrineBundle\Registry;
-
-use Doctrine\ORM\NoResultException;
-use Doctrine\ORM\Mapping\MappingException;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /*
  * This file is part of the Symfony framework.
@@ -21,14 +18,25 @@ use Doctrine\ORM\Mapping\MappingException;
 
 /**
  * DoctrineConverter.
+ * 
+ * The DoctrineConverter can recieve three options in the options array:
+ *   entity_manager - the entity manager to user for fetching the object
+ * 2. request_atttribute - the name of the attribute you should use to fetch the item (Defaults to 'id').
+ * 3. object_attribute - the name of the object attribute used for fetching the object (Defaults to false - i.e. use the objects natural ID.).
+ * 
+ * 
+ * 
  *
  * @author     Fabien Potencier <fabien@symfony.com>
  */
 class DoctrineParamConverter implements ParamConverterInterface
 {
+    /**
+     * @var ManagerRegistry
+     */
     protected $registry;
 
-    public function __construct(Registry $registry = null)
+    public function __construct(ManagerRegistry $registry = null)
     {
         $this->registry = $registry;
     }
@@ -37,35 +45,44 @@ class DoctrineParamConverter implements ParamConverterInterface
     {
         $class = $configuration->getClass();
         $options = $this->getOptions($configuration);
-
-        // find by identifier?
-        if (false === $object = $this->find($class, $request, $options)) {
-            // find by criteria
-            if (false === $object = $this->findOneBy($class, $request, $options)) {
-                throw new \LogicException('Unable to guess how to get a Doctrine instance from the request information.');
+        
+        $attribute = (isset($options['request_attribute']) ? $options['request_attribute'] : 'id' );
+        $queryAttribute = (isset($options['object_attribute']) ? $options['object_attribute'] : false );
+        
+        if (false === $queryAttribute) {
+            // find by identifier?
+            $object = $this->find($class, $request, $options, $attribute);
+            if (false === $object ) {
+                // find by criteria
+                if (false === $object = $this->findOneBy($class, $request, $options)) {
+                    throw new \LogicException('Unable to guess how to get a Doctrine instance from the request information.');
+                }
             }
+        } else {
+            $criteria = array($queryAttribute => $request->attributes->get($attribute));
+            $object = $this->registry->getRepository($class, $options['entity_manager'])->findOneBy($criteria);
         }
 
         if (null === $object && false === $configuration->isOptional()) {
             throw new NotFoundHttpException(sprintf('%s object not found.', $class));
         }
-
         $request->attributes->set($configuration->getName(), $object);
+
+        return true;
     }
 
-    protected function find($class, Request $request, $options)
+    protected function find($class, Request $request, $options, $attribute)
     {
-        if (!$request->attributes->has('id')) {
+        if (!$request->attributes->has($attribute)) {
             return false;
         }
-
-        return $this->registry->getRepository($class, $options['entity_manager'])->find($request->attributes->get('id'));
+        return $this->registry->getRepository($class, $options['entity_manager'])->find($request->attributes->get($attribute));
     }
 
     protected function findOneBy($class, Request $request, $options)
     {
         $criteria = array();
-        $metadata = $this->registry->getEntityManager($options['entity_manager'])->getClassMetadata($class);
+        $metadata = $this->registry->getManager($options['entity_manager'])->getClassMetadata($class);
         foreach ($request->attributes->all() as $key => $value) {
             if ($metadata->hasField($key)) {
                 $criteria[$key] = $value;
@@ -92,19 +109,17 @@ class DoctrineParamConverter implements ParamConverterInterface
         $options = $this->getOptions($configuration);
 
         // Doctrine Entity?
-        try {
-            $this->registry->getEntityManager($options['entity_manager'])->getClassMetadata($configuration->getClass());
-
-            return true;
-        } catch (MappingException $e) {
-            return false;
-        }
+        return ! $this->registry->getManager($options['entity_manager'])
+                                ->getMetadataFactory()
+                                ->isTransient($configuration->getClass());
     }
 
     protected function getOptions(ConfigurationInterface $configuration)
     {
         return array_replace(array(
             'entity_manager' => 'default',
+            'request_atttribute' => 'id',
+            'object_attribute' => false
         ), $configuration->getOptions());
     }
 }
---
+++ Tests/Request/ParamConverter/DoctrineParamConverterTest.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Sensio\Bundle\FrameworkExtraBundle\Tests\Request\ParamConverter;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\DoctrineParamConverter;
+
+class DoctrineParamConverterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Doctrine\Common\Persistence\ManagerRegistry
+     */
+    private $manager;
+    
+    /**
+     * @var DoctrineParamConverter
+     */
+    private $converter;
+    
+    public function setUp()
+    {
+            if (!interface_exists('Doctrine\Common\Persistence\ManagerRegistry')) {
+            $this->markTestSkipped('Missing Doctrine\Common\Persistence\ManagerRegistry');
+        }
+
+        $this->manager = $this->getMock('Doctrine\Common\Persistence\ManagerRegistry');
+        $this->converter = new DoctrineParamConverter($this->manager);
+        
+        $this->objectRepository = $this->getMock('Doctrine\Common\Persistence\ObjectRepository');
+        $this->objectManager = $this->getMock('Doctrine\Common\Persistence\ObjectManager');
+        $this->manager->expects($this->any())->method('getRepository')
+                      ->will($this->returnValue($this->objectRepository));
+        $this->manager->expects($this->any())->method('getManager')
+                      ->will($this->returnValue($this->objectManager));
+        
+    }
+    
+    public function createConfiguration($class = null, array $options = null)
+    {
+        $config = $this->getMock(
+            'Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface', array(
+            'getClass', 'getAliasName', 'getOptions', 'isOptional', 'getName', 
+        ));
+        if ($options !== null) {
+            $config->expects($this->once())
+                   ->method('getOptions')
+                   ->will($this->returnValue($options));
+        }
+        if ($class !== null) {
+            $config->expects($this->any())
+                   ->method('getClass')
+                   ->will($this->returnValue($class));
+        }
+        return $config;
+    }
+    
+    public function testApplyWithNoIdAndData()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration(null, array());
+        $this->setExpectedException('LogicException');
+        $this->converter->apply($request, $config);
+    }
+
+    public function testNonOptionalNotFound()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration(null, array());
+        $config->expects($this->once())
+               ->method("isOptional")
+               ->will($this->returnValue(false));
+                
+        $request->attributes->set("id", "34");
+        
+        $this->setExpectedException('Symfony\Component\HttpKernel\Exception\NotFoundHttpException');
+        $this->converter->apply($request, $config);
+    }
+    /**
+     * Old behaviour, we use the id attribute to query for the user by id
+     */
+    public function testApplyDefault()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration("User", array());
+        
+        $request->attributes->set("id", "testName");
+        
+        $this->objectRepository->expects($this->once())->method('find')
+                ->with($this->equalTo("testName"))->will($this->returnValue("object"))
+                ;
+        $this->objectRepository->expects($this->never())->method('findBy');
+        
+        $ret = $this->converter->apply($request, $config);
+        
+        $this->assertTrue($ret, "We should have found an object");
+        $this->assertEquals("object", $request->attributes->get($config->getName()));
+    }
+
+    public function testApplyWithRequestAttribute()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration("User", array('request_attribute' => 'name'));
+        
+        $request->attributes->set("name", "testName");
+        
+        $this->objectRepository->expects($this->once())->method('find')
+                ->with($this->equalTo("testName"))->will($this->returnValue("object"))
+                ;
+        $this->objectRepository->expects($this->never())->method('findBy');
+        
+        $ret = $this->converter->apply($request, $config);
+        
+        $this->assertTrue($ret, "We should have found an object");
+        $this->assertEquals("object", $request->attributes->get($config->getName()));
+    }
+
+        public function testApplyWithRequestAttributeAndQueryAttribute()
+    {
+        $request = new Request();
+        $config = $this->createConfiguration("User", array('request_attribute' => 'name'));
+        
+        $request->attributes->set("name", "testName");
+        
+        $this->objectRepository->expects($this->once())->method('find')
+                ->with($this->equalTo("testName"))->will($this->returnValue("object"))
+                ;
+        $this->objectRepository->expects($this->never())->method('findBy');
+        
+        $ret = $this->converter->apply($request, $config);
+        
+        $this->assertTrue($ret, "We should have found an object");
+        $this->assertEquals("object", $request->attributes->get($config->getName()));
+    }
+
+    public function testSupports()
+    {
+        $config = $this->createConfiguration('stdClass', array());
+        $metadataFactory = $this->getMock('Doctrine\Common\Persistence\Mapping\ClassMetadataFactory');
+        $metadataFactory->expects($this->once())
+                        ->method('isTransient')
+                        ->with($this->equalTo('stdClass'))
+                        ->will($this->returnValue( false ));
+        
+        $this->objectManager->expects($this->once())
+                      ->method('getMetadataFactory')
+                      ->will($this->returnValue($metadataFactory));
+        
+        $this->manager->expects($this->once())
+                      ->method('getManager')
+                      ->with($this->equalTo('default'))
+                      ->will($this->returnValue($this->objectManager));
+        
+        $ret = $this->converter->supports($config);
+        
+        
+        
+        $this->assertTrue($ret, "Should be supported");
+        
+    }
+}
